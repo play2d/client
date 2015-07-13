3f9b03873fcdb50d7dc4b30f1a879b61ccdbfa3e
Starkkz <matiasahermosilla@gmail.com>
2015-07-13 17:11:44 -0300

    Functional Combobox

--- gui/Combobox.lua
+++ gui/Combobox.lua
@@ -17,6 +17,9 @@ function TCombobox.New()
 	return setmetatable({}, TComboboxMetatable)
 end
 
+function TCombobox:OnSelect(Index)
+end
+
 function TCombobox:Init()
 	self.Items = {}
 	return self
@@ -33,6 +36,7 @@ function TCombobox:SetItem(Index, Item)
 	if not self.Items[Index] then
 		self.ItemCount = self.ItemCount + 1
 	end
+	self.Items[Index] = Item
 end
 
 function TCombobox:RemoveItem(Index)
@@ -42,13 +46,28 @@ function TCombobox:RemoveItem(Index)
 	end
 end
 
-function TCombobox:MouseClicked(x, y)
+function TCombobox:MouseClicked(mx, my)
 	if not self.Disabled and not self.Hidden then
+		local x, y = self:x(), self:y()
 		self.Dropped = mil
-		self.Grabbed = {x = x - self:x(), y = y - self:y()}
+		self.Grabbed = {x = mx - x, y = my - y}
 		self:OnClick(self.Grabbed.x, self.Grabbed.y)
 		self:SetHoverAll()
 		self.Open = not self.Open
+		
+		if not self.Open then
+			local HeightOffset = 0
+			local Width, Height = self:Width(), self:Height()
+			local FontHeight = self:GetFont():getHeight()
+			for Index, Item in pairs(self.Items) do
+				if self.Selected ~= Index and self:MouseHoverArea(0, Height + HeightOffset, Width, FontHeight + 4.5) then
+					self:OnSelect(Index)
+					self.Selected = Index
+					self.Text = Item
+				end
+				HeightOffset = HeightOffset + FontHeight + 5
+			end
+		end
 	end
 end
 
@@ -66,8 +85,14 @@ function TCombobox:Render(dt)
 		love.graphics.setColor(unpack(Theme.Background))
 		love.graphics.rectangle("fill", x + 1, y + 1, Width - 2, Height - 2)
 		
+		if self.Text then
+			love.graphics.setColor(unpack(Theme.Text))
+			love.graphics.print(self.Text, x + 2.5, y + (Height - Font:getHeight())/2)
+		end
+		
 		if self.Open then
-			local BoxHeight = self.ItemCount * (Font:getHeight() + 5)
+			local FontHeight = Font:getHeight()
+			local BoxHeight = self.ItemCount * (FontHeight + 5)
 			love.graphics.setScissor(x, y + Height, Width, BoxHeight)
 			
 			love.graphics.setColor(unpack(Theme.Border))
@@ -75,6 +100,22 @@ function TCombobox:Render(dt)
 			
 			love.graphics.setColor(unpack(Theme.Background))
 			love.graphics.rectangle("fill", x + 1, y + Height + 1, Width - 2, BoxHeight - 2)
+			
+			love.graphics.setColor(unpack(Theme.Text))
+			local HeightOffset = 0
+			for Index, Item in pairs(self.Items) do
+				love.graphics.print(Item, x + 2.5, y + Height + HeightOffset + 2.5)
+				if self.Selected == Index then
+					love.graphics.setColor(unpack(Theme.Selected))
+					love.graphics.rectangle("fill", x + 1, y + Height + HeightOffset, Width, FontHeight + 5)
+					love.graphics.setColor(unpack(Theme.Text))
+				elseif self:MouseHoverArea(0, Height + HeightOffset, Width, FontHeight + 4.5) then
+					love.graphics.setColor(unpack(Theme.Hover))
+					love.graphics.rectangle("fill", x + 1, y + Height + HeightOffset, Width, FontHeight + 5)
+					love.graphics.setColor(unpack(Theme.Text))
+				end
+				HeightOffset = HeightOffset + FontHeight + 5
+			end
 		end
 	end
 end
