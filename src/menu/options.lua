a6c490a3695d86c125dd43f01e300a08449f074a
Starkkz <matiasahermosilla@gmail.com>
2015-07-25 13:33:19 -0300

    Added name textfield

--- src/menu/options.lua
+++ src/menu/options.lua
@@ -1,51 +1,55 @@
 function game.ui.OpenOptionsMenu()
-	game.ui.Options.Hidden = nil
+	game.ui.Options.Window.Hidden = nil
 end
 
 local function InitializeOptionsMenu()
-	game.ui.Options = gui.CreateWindow(language.get("gui_label_options"), 120, 10, 670, 580, game.ui.Desktop, true)
-	game.ui.Options.Hidden = true
+	game.ui.Options = {}
+	game.ui.Options.Window = gui.CreateWindow(language.get("gui_label_options"), 120, 10, 670, 580, game.ui.Desktop, true)
+	game.ui.Options.Window.Hidden = true
 	
-	game.ui.OptionsTab = gui.CreateTabber(10, 35, game.ui.Options)
-	game.ui.OptionsTab:AddItem(language.get("gui_options_tab_player"))
-	game.ui.OptionsTab:AddItem(language.get("gui_options_tab_controls"))
-	game.ui.OptionsTab:AddItem(language.get("gui_options_tab_game"))
-	game.ui.OptionsTab:AddItem(language.get("gui_options_tab_graphics"))
-	game.ui.OptionsTab:AddItem(language.get("gui_options_tab_sound"))
-	game.ui.OptionsTab:AddItem(language.get("gui_options_tab_net"))
-	game.ui.OptionsTab:AddItem(language.get("gui_options_tab_more"))
+	game.ui.Options.Tab = gui.CreateTabber(10, 35, game.ui.Options.Window)
+	game.ui.Options.Tab:AddItem(language.get("gui_options_tab_player"))
+	game.ui.Options.Tab:AddItem(language.get("gui_options_tab_controls"))
+	game.ui.Options.Tab:AddItem(language.get("gui_options_tab_game"))
+	game.ui.Options.Tab:AddItem(language.get("gui_options_tab_graphics"))
+	game.ui.Options.Tab:AddItem(language.get("gui_options_tab_sound"))
+	game.ui.Options.Tab:AddItem(language.get("gui_options_tab_net"))
+	game.ui.Options.Tab:AddItem(language.get("gui_options_tab_more"))
 	
-	function game.ui.OptionsTab:OnSelect(Index)
-		game.ui.OptionsPanels[self.Selected].Hidden = true
-		game.ui.OptionsPanels[Index].Hidden = nil
+	function game.ui.Options.Tab:OnSelect(Index)
+		game.ui.Options.Panels[self.Selected].Hidden = true
+		game.ui.Options.Panels[Index].Hidden = nil
 	end
 	
-	game.ui.OptionsPanels = {}
+	game.ui.Options.Panels = {}
 	
 	-- Player panel
-	game.ui.OptionsPanels[1] = gui.CreatePanel(language.get("gui_options_tab_player"), 10, 60, 650, 510, game.ui.Options)
-	gui.CreateLabel(language.get("gui_options_player_name"), 20, 30, game.ui.OptionsPanels[1])
+	game.ui.Options.Panels[1] = gui.CreatePanel(language.get("gui_options_tab_player"), 10, 60, 650, 510, game.ui.Options.Window)
+	gui.CreateLabel(language.get("gui_options_player_name"), 20, 30, game.ui.Options.Panels[1])
+	
+	game.ui.Options.NameField = gui.CreateTextfield(20, 50, 200, 20, game.ui.Options.Panels[1])
+	game.ui.Options.NameField:SetText(config["name"])
 	
 	-- Controls panel
-	game.ui.OptionsPanels[2] = gui.CreatePanel(language.get("gui_options_tab_controls"), 10, 60, 650, 510, game.ui.Options)
+	game.ui.Options.Panels[2] = gui.CreatePanel(language.get("gui_options_tab_controls"), 10, 60, 650, 510, game.ui.Options.Window)
 	
 	-- Game panel
-	game.ui.OptionsPanels[3] = gui.CreatePanel(language.get("gui_options_tab_game"), 10, 60, 650, 510, game.ui.Options)
+	game.ui.Options.Panels[3] = gui.CreatePanel(language.get("gui_options_tab_game"), 10, 60, 650, 510, game.ui.Options.Window)
 	
 	-- Graphics panel
-	game.ui.OptionsPanels[4] = gui.CreatePanel(language.get("gui_options_tab_graphics"), 10, 60, 650, 510, game.ui.Options)
+	game.ui.Options.Panels[4] = gui.CreatePanel(language.get("gui_options_tab_graphics"), 10, 60, 650, 510, game.ui.Options.Window)
 	
 	-- Sound panel
-	game.ui.OptionsPanels[5] = gui.CreatePanel(language.get("gui_options_tab_sound"), 10, 60, 650, 510, game.ui.Options)
+	game.ui.Options.Panels[5] = gui.CreatePanel(language.get("gui_options_tab_sound"), 10, 60, 650, 510, game.ui.Options.Window)
 	
 	-- Net panel
-	game.ui.OptionsPanels[6] = gui.CreatePanel(language.get("gui_options_tab_net"), 10, 60, 650, 510, game.ui.Options)
+	game.ui.Options.Panels[6] = gui.CreatePanel(language.get("gui_options_tab_net"), 10, 60, 650, 510, game.ui.Options.Window)
 	
 	-- More Panel
-	game.ui.OptionsPanels[7] = gui.CreatePanel(language.get("gui_options_tab_more"), 10, 60, 650, 510, game.ui.Options)
+	game.ui.Options.Panels[7] = gui.CreatePanel(language.get("gui_options_tab_more"), 10, 60, 650, 510, game.ui.Options.Window)
 	
-	for i = 2, #game.ui.OptionsPanels do
-		game.ui.OptionsPanels[i].Hidden = true
+	for i = 2, #game.ui.Options.Panels do
+		game.ui.Options.Panels[i].Hidden = true
 	end
 	
 	InitializeOptionsMenu = nil
--- sys/commands/name.lua
+++ sys/commands/name.lua
@@ -2,4 +2,7 @@ local Name = ...
 
 if type(Name) == "string" then
 	config["name"] = Name
+	if game.ui.Options then
+		game.ui.Options.NameField:SetText(config["name"])
+	end
 end
