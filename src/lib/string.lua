4cd3b3b81904f55cc4f92f51fcdc168dbc9f8c0a
Starkkz <matiasahermosilla@gmail.com>
2015-12-01 17:28:04 -0300

    string:ReadNumber and string:WriteNumber

--- src/lib/string.lua
+++ src/lib/string.lua
@@ -40,6 +40,62 @@ function string:ReadInt()
 	return self:byte(1) + self:byte(2) * 256 + self:byte(3) * (256^2) + self:byte(4) * (256^3), self:sub(5)
 end
 
+function string:ReadNumber()
+	local ByteArray = ffi.new("unsigned char [8]")
+	for i = 0, 7 do
+		ByteArray[i], self = self:ReadByte()
+	end
+	
+	local BitArray = ffi.new("bool [64]")
+	for Index = 0, 7 do
+		for Offset = 7, 0, -1 do
+			local Bit = 2 ^ Offset
+			if ByteArray[Index] >= Bit then
+				ByteArray[Index] = ByteArray[Index] - Bit
+				BitArray[Index * 8 + Offset] = true
+			end
+		end
+	end
+	
+	local Exponent = 0
+	for BitID = 1, 11 do
+		if BitArray[BitID + 1] then
+			Exponent = Exponent + 2 ^ (BitID - 1)
+		end
+	end
+	
+	local Fraction = 0
+	for BitID = 1, 51 do
+		if BitArray[BitID + 12] then
+			Fraction = Fraction + (1/2) ^ (BitID - 1)
+		end
+	end
+	
+	if BitArray[0] then
+		Fraction = -Fraction
+	end
+	return Fraction * 2 ^ Exponent
+end
+
+function string:ReadFloat()
+	local Integer, self = self:ReadInt()
+	local Fraction = 0
+	
+	for ByteID = 1, 4 do
+		local ByteMult, Byte = 256 ^ (ByteID - 1)
+		
+		Byte, self = self:ReadByte()
+		for BitID = 8, 1, -1 do
+			local Bit = 2 ^ (BitID - 1)
+			if Byte >= Bit then
+				Byte = Byte - Bit
+				Fraction = Fraction + (1/Bit) * (1/ByteMult)
+			end
+		end
+	end
+	return Integer + Fraction, self:sub(9)
+end
+
 function string:ReadString(Length)
 	return self:sub(1, Length), self:sub(Length + 1)
 end
@@ -78,15 +134,49 @@ function string:WriteInt(n)
 end
 
 function string:WriteNumber(n)
-	-- 1 bit = sign of n
-	-- 11 bit = exponent of n
-	-- 52 bit = fraction of n
-	-- n = sign * fraction * 2 ^ (exponent of n - 1023)
+	local BitArray = ffi.new("bool [64]")
+	if n < 0 then
+		BitArray[0] = true
+		n = math.floor(n)
+	end
+	
+	-- math.frexp(n) = Fraction -> n = Fraction * 2 ^ Exponent
+	local Fraction = math.frexp(n)
+	
+	-- n = Fraction * 2 ^ Exponent -> n/Fraction = 2 ^ Exponent
+	-- log10(2^Exponent)/log10(2) = Exponent * log10(2) / log10(2) = Exponent
+	local Exponent = math.log10(n/Fraction)/0.30102999566398 -- math.log10(2) = 0.30102999566398
+	
+	for BitID = 11, 1, -1 do
+		local Bit = 2 ^ (BitID - 1)
+		if Exponent >= Bit then
+			Exponent = Exponent - Bit
+			BitArray[BitID + 1] = true
+		end
+	end
+	
+	for BitID = 1, 51 do
+		if Fraction >= 1 then
+			Fraction = Fraction - 1
+			BitArray[BitID + 12] = true
+		end
+		Fraction = Fraction * 2
+	end
+	
+	local ByteArray = ffi.new("unsigned char [8]")
+	for Index = 0, 7 do
+		for Offset = 0, 7 do
+			local Bit = BitArray[Index * 8 + Offset]
+			if Bit then
+				ByteArray[Index] = ByteArray[Index] + 2 ^ Offset 
+			end
+		end
+	end
+	return self .. ffi.string(ByteArray, 8)
 end
 
 function string:WriteFloat(n)
-	local Integer = math.floor(n)
-	local Fraction = n - Integer
+	local Integer, Fraction = math.modf(n)
 	
 	self = self:WriteInt(Integer)
 	for ByteID = 1, 4 do
@@ -101,25 +191,6 @@ function string:WriteFloat(n)
 		self = self:WriteByte(Byte)
 	end
 	return self
-end
-
-function string:ReadFloat()
-	local Integer, self = self:ReadInt()
-	local Fraction = 0
-	
-	for ByteID = 1, 4 do
-		local ByteMult, Byte = 256 ^ (ByteID - 1)
-		
-		Byte, self = self:ReadByte()
-		for BitID = 8, 1, -1 do
-			local Bit = 2 ^ (BitID - 1)
-			if Byte >= Bit then
-				Byte = Byte - Bit
-				Fraction = Fraction + (1/Bit) * (1/ByteMult)
-			end
-		end
-	end
-	return Integer + Fraction, self:sub(9)
 end
 
 function string:WriteString(String)
