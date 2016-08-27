bdb666a8cfc936e7523f4328105b7b3cbdf78436
Starkkz <matiasahermosilla@gmail.com>
2016-08-27 17:19:52 -0300

    Better code organization

---
+++ main.lua
@@ -0,0 +1,37 @@
+PLAY2D = require("src")
+
+function love.load(...)
+	PLAY2D.load(...)
+end
+
+function love.update(...)
+	PLAY2D.update(...)
+end
+
+function love.draw(...)
+	PLAY2D.draw(...)
+end
+
+function love.mousepressed(...)
+	PLAY2D.mousepressed(...)
+end
+
+function love.mousereleased(...)
+	PLAY2D.mousereleased(...)
+end
+
+function love.mousemoved(...)
+	PLAY2D.mousemoved(...)
+end
+
+function love.wheelmoved(...)
+	PLAY2D.wheelmoved(...)
+end
+
+function love.keypressed(...)
+	PLAY2D.keypressed(...)
+end
+
+function love.textinput(...)
+	PLAY2D.textinput(...)
+end
---
+++ src/init.lua
@@ -0,0 +1,33 @@
+local PLAY2D = {}
+local Path = ...
+
+PLAY2D.gui = require(Path..".gui")
+
+function PLAY2D.load()
+end
+
+function PLAY2D.update(dt)
+end
+
+function PLAY2D.draw()
+end
+
+function PLAY2D.mousepressed(...)
+end
+
+function PLAY2D.mousereleased(...)
+end
+
+function PLAY2D.mousemoved(...)
+end
+
+function PLAY2D.wheelmoved(...)
+end
+
+function PLAY2D.keypressed(...)
+end
+
+function PLAY2D.textinput(...)
+end
+
+return PLAY2D
