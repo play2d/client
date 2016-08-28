da12ca588a73a490649740d44791e60f5e35857c
Starkkz <matiasahermosilla@gmail.com>
2016-08-28 14:00:25 -0300

    Changed update command

--- UPDATE SUBMODULE.bat
+++ UPDATE SUBMODULE.bat
@@ -1,5 +1,5 @@
 git submodule update --recursive
-git submodule foreach git pull
+git submodule foreach git pull origin master
 git add src/gui
 git commit -m "updating submodules to master"
 git push
