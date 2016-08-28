534b72251edad819a93fdc9b30c7acac7474245f
Starkkz <matiasahermosilla@gmail.com>
2016-08-28 10:22:26 -0300

    Changed splash object

--- src/interface/Desktop.lua
+++ src/interface/Desktop.lua
@@ -2,7 +2,10 @@ local Path, PLAY2D, Interface = ...
 local Desktop = {}
 
 function Desktop.load()
-	PLAY2D.gui.Desktop:SetSplash(love.graphics.newImage("gfx/splash.png"))
+	local Width, Height = PLAY2D.gui.Desktop:GetDimensions()
+	
+	PLAY2D.Main = PLAY2D.gui.create("Panel", 0, 0, Width, Height, PLAY2D.gui.Desktop)
+	PLAY2D.Main:SetSplash(love.graphics.newImage("gfx/splash.png"))
 	
 	Desktop.load = nil
 end
