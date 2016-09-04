5ae6e21c598291e042ab0adc6dcb0c18f980310f
_Yank <saskslay@gmail.com>
2016-09-04 12:07:33 +0000

    Adapted the assets to the new directory structure Added the new directory structure reference on doc dir

--- src/interface/Desktop.lua
+++ src/interface/Desktop.lua
@@ -34,7 +34,7 @@ function Desktop.load()
 	local Width, Height = PLAY2D.gui.Desktop:GetDimensions()
 	
 	PLAY2D.Main = PLAY2D.gui.create("Panel", 0, 0, Width, Height, PLAY2D.gui.Desktop)
-	PLAY2D.Main:SetSplash(love.graphics.newImage("gfx/splash.png"))
+	PLAY2D.Main:SetSplash(love.graphics.newImage("assets/gfx/splash.png"))
 	
 	Desktop.load = nil
 	
